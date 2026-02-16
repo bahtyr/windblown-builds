1cfd64aa4b4dd820b53b68423fd034a6c5e5ecbd
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2026-02-15 22:23:20 -0500

    first gift

--- src/main/java/org/example/Gift.java
+++ src/main/java/org/example/Gift.java
@@ -16,5 +16,6 @@ public class Gift {
 //                ", description='" + description + '\'' + skip
                 ", richDescription=" + richDescription +
                 '}'; // todo convert this to json like format
+        // todo if very easy, beautify json
     }
 }
