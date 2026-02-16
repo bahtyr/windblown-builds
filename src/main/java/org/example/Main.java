fb33f095daf05bd89738b83e14c7cead73f5a774
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2026-02-16 05:47:13 -0500

    first gift

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,15 +16,15 @@ public class Main {
         WebDriver driver = new FirefoxDriver();
         driver.get(GiftsPage.URL);
 
-        String section = "General Gifts";
+        String section = "Scythe Gifts";
 
         WebElement table = driver.findElement(GiftsPage.tableOfSection(section));
-        WebElement row = table.findElements(By.tagName("tr")).get(6);
+        WebElement row = table.findElements(By.tagName("tr")).get(1);
         List<WebElement> rows = row.findElements(By.tagName("td"));
 
         Gift a = new Gift();
         a.imageUrl = rows.get(0).findElement(By.tagName("img")).getAttribute("src");
-        a.name = rows.get(1).getText();
+        a.name = rows.get(0).getText();
         a.category = section.replace(" Gifts", "").trim();
 
         WebElement rowDescription = rows.get(2);
@@ -32,6 +32,101 @@ public class Main {
                 .map(RichTextToken::toJsonLikeString)
                 .collect(Collectors.joining(",", "[", "]"));
 
+                /*
+
+        todo RichTextParser
+        input
+
+<td style="">
+  <span>Every <b>250</b>
+    <span class="tooltip" style="position: relative; display: inline;">
+      <b>
+        <a href="/wiki/Base_Damage" class="mw-redirect" title="">
+          <span style="color: #ffffff;">Base Damage</span>
+        </a>
+      </b>
+      <span class="tooltiptext" style="width: 25em;">...</span>
+    </span> you deal, spawn a <span class="tooltip" style="position: relative; display: inline;">
+      <a href="/wiki/Scythe" title="">
+        <img alt="Scythe Icon.png" src="/images/thumb/Scythe_Icon.png/20px-Scythe_Icon.png?16e570" decoding="async" loading="lazy" width="20" height="20" data-file-width="128" data-file-height="128">
+      </a>
+      <b>
+        <a href="/wiki/Scythe" class="mw-redirect" title="">
+          <span style="color: #5f9fff;">Scythe</span>
+        </a>
+      </b>
+      <span class="tooltiptext" style="width: 25em; left: 1132px; top: 426px;">...</span>
+    </span> for 5s ( <span class="tooltip" style="position: relative; display: inline;">
+      <a href="/wiki/Auto_Scaling" title="">
+        <img alt="Auto Scaling Icon.png" src="/images/thumb/Auto_Scaling_Icon.png/20px-Auto_Scaling_Icon.png?ceb0bb" decoding="async" loading="lazy" width="20" height="20" data-file-width="128" data-file-height="128">
+      </a>
+      <b>
+        <a href="/wiki/Auto_Scaling" class="mw-redirect" title="">
+          <span style="color: #ffffff;"></span>
+        </a>
+      </b>
+      <span class="tooltiptext" style="width: 25em;">...</span>
+    </span>
+    <b>
+      <span style="color: #00ff22;">10</span>
+    </b> damage). </span>
+</td>
+
+        expected output
+
+{
+  "imageUrl": "https://windblown.wiki.gg/images/thumb/Damage_Scythe_Icon.png/48px-Damage_Scythe_Icon.png?2431b1",
+  "name": "",
+  "category": "Scythe",
+  "richDescription": [
+    {
+      "t": "text",
+      "text": "Every "
+    },
+    {
+      "bold": true
+      "text": "250"
+    },
+    {
+      "t": "entity",
+      "text": "Base Damage",
+      "href": "/wiki/Base_Damage",
+      "color": "#ffffff"
+    },
+    {
+      "t": "text",
+      "text": " you deal, spawn a "
+    },
+    {
+      "t": "entity",
+      "text": "Scythe",
+      "href": "/wiki/Scythe",
+      "icon": "/images/thumb/Scythe_Icon.png/20px-Scythe_Icon.png?16e570",
+      "color": "#5f9fff"
+    },
+    {
+      "t": "text",
+      "text": " for 5s ("
+    },
+    {
+      "t": "entity",
+      "text": "",
+      "href": "/wiki/Auto_Scaling",
+      "icon": "/images/thumb/Auto_Scaling_Icon.png/20px-Auto_Scaling_Icon.png?ceb0bb",
+      "color": "#ffffff"
+    },
+    {
+      "text": "10",
+      "color": "#00ff22"
+      "bold": true
+    },
+    {
+      "t": "text",
+      "text": " damage)."
+    }
+  ]
+}
+                 */
         System.out.print(a);
 
         driver.quit();
