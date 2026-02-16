a18b74589319781f0f8533e0aac4ba855ebb3c95
Bahtiyar Ceylan <50676218+bahtyr@users.noreply.github.com>
2026-02-16 06:41:19 -0500

    multiple gifts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
+
+    static ArrayList<Gift> gifts = new ArrayList<>();
+    static List<String> sections = List.of(
+            "Scythe Gifts",
+            "Curse Gifts"
+    );
 
 
     static void main() throws InterruptedException {
@@ -16,25 +23,30 @@ public class Main {
         WebDriver driver = new FirefoxDriver();
         driver.get(GiftsPage.URL);
 
-        String section = "Scythe Gifts";
+        for (String section : sections) {
+            WebElement table = driver.findElement(GiftsPage.tableOfSection(section));
+            WebElement row = table.findElements(By.tagName("tr")).get(2);
+            gifts.add(parseGift(row, section));
+        }
 
-        WebElement table = driver.findElement(GiftsPage.tableOfSection(section));
-        WebElement row = table.findElements(By.tagName("tr")).get(2);
-        List<WebElement> td = row.findElements(By.tagName("td"));
-
-        Gift a = new Gift();
-        a.imageUrl = td.get(0).findElement(By.tagName("img")).getAttribute("src");
-        a.name = td.get(0).getText();
-        a.category = section.replace(" Gifts", "").trim();
-        WebElement rowDescription = td.get(2);
-        a.description = rowDescription.getText();
-        a.richDescription = RichTextParser.parse(rowDescription).stream()
-                .map(RichTextToken::toJsonLikeString)
-                .collect(Collectors.joining(",", "[", "]"));
-        System.out.print(a);
+        System.out.print(gifts);
 
         driver.quit();
 
+    }
+
+    static Gift parseGift(WebElement row, String section) {
+        Gift gift = new Gift();
+        List<WebElement> td = row.findElements(By.tagName("td"));
+        gift.imageUrl = td.get(0).findElement(By.tagName("img")).getAttribute("src");
+        gift.name = td.get(0).getText();
+        gift.category = section.replace(" Gifts", "").trim();
+        WebElement rowDescription = td.get(2);
+        gift.description = rowDescription.getText();
+        gift.richDescription = RichTextParser.parse(rowDescription).stream()
+                .map(RichTextToken::toJsonLikeString)
+                .collect(Collectors.joining(",", "[", "]"));
+        return gift;
     }
 
 }
